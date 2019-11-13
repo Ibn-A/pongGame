45197a38de1a07b660acf4913dc44f4f6310a08b
admin <admin@MacBook-Pro-de-Elsa.local>
2019-11-13 10:23:02 +0100

    paddle B up&down_setup

--- pong.py
+++ pong.py
@@ -47,11 +47,23 @@ def paddle_a_down():
     y -= 20
     paddle_a.sety(y)
 
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
 
 #Keyboard binding
 wn.listen()
 wn.onkey(paddle_a_up, "w")
 wn.onkey(paddle_a_down, "s")
+wn.onkey(paddle_b_up, "Up")
+wn.onkey(paddle_b_down, "Down")
 
 #Main game loop
 while True:
